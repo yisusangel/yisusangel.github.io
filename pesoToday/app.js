73bc8d2183368148d018bade36365c7071b9451b
yisusangel <jesusangel_jimenez@hotmail.com>
2017-09-22 15:13:26 -0300

    mas responsive

--- pesoToday/app.js
+++ pesoToday/app.js
@@ -32,8 +32,8 @@ var app = new Vue({
     var that = this;
     function valoresBase(){
       that.tasasConversion.normal = parseFloat(Math.round(that.tasas.bolivar/that.tasas.peso * 100) / 100).toFixed(2);
-      that.tasasConversion.recPeso = parseFloat(Math.round(((that.tasas.bolivar/that.tasas.peso)/1.3) * 100) / 100).toFixed(2);
-      that.tasasConversion.recBolivar = parseFloat(Math.round((that.tasas.bolivar/that.tasas.peso)*1.1 * 100) / 100).toFixed(2);
+      that.tasasConversion.recPeso = parseFloat(Math.round(((that.tasas.bolivar/that.tasas.peso)/1.25) * 100) / 100).toFixed(2);
+      that.tasasConversion.recBolivar = parseFloat(Math.round((that.tasas.bolivar/that.tasas.peso)*1.13 * 100) / 100).toFixed(2);
 
       that.bolivarConv.recPeso = that.tasasConversion.recPeso;
       that.bolivarConv.recBolivar = that.tasasConversion.recBolivar;
@@ -49,7 +49,7 @@ var app = new Vue({
     axios.get("http://www.apilayer.net/api/live?access_key=0a722f2dcdd9b68c52dd508df4db2e73&format=1")
     .then(function (response) {
       that.tasas.peso = parseFloat(Math.round(response.data.quotes["USDCLP"] * 100) / 100).toFixed(2);
-      that.tasas.pesoCompra = parseFloat(Math.round((that.tasas.peso*1.01)* 100) / 100).toFixed(2);
+      that.tasas.pesoCompra = parseFloat(Math.round((that.tasas.peso*1.02)* 100) / 100).toFixed(2);
       that.valoresDolar.peso = that.tasas.peso;
       that.valoresDolar.pesoCompra = that.tasas.pesoCompra;
       valoresBase();
